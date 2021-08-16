f2c38b7fab75fb86523005474d633fd7577cbe61
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-08-16 12:50:11 +0700

    Add files via upload

---
+++ OnScreenKeyBoard/OnScreenKeyBoard/frmOnScreenKeyBoard.designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOnScreenKeyBoard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShowKeyboard = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnHideKeyboard = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnShowKeyboard
+        '
+        Me.btnShowKeyboard.Location = New System.Drawing.Point(22, 14)
+        Me.btnShowKeyboard.Name = "btnShowKeyboard"
+        Me.btnShowKeyboard.Size = New System.Drawing.Size(215, 45)
+        Me.btnShowKeyboard.TabIndex = 0
+        Me.btnShowKeyboard.Text = "Show keyboard"
+        Me.btnShowKeyboard.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(22, 78)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(510, 243)
+        Me.TextBox1.TabIndex = 2
+        '
+        'btnHideKeyboard
+        '
+        Me.btnHideKeyboard.Location = New System.Drawing.Point(317, 14)
+        Me.btnHideKeyboard.Name = "btnHideKeyboard"
+        Me.btnHideKeyboard.Size = New System.Drawing.Size(215, 45)
+        Me.btnHideKeyboard.TabIndex = 3
+        Me.btnHideKeyboard.Text = "Hide keyboard"
+        Me.btnHideKeyboard.UseVisualStyleBackColor = True
+        '
+        'frmOnScreenKeyBoard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(555, 333)
+        Me.Controls.Add(Me.btnHideKeyboard)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnShowKeyboard)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "frmOnScreenKeyBoard"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Demo On Screen Keyboard - coDe bY: Thongkorn Tubtimkrob"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnShowKeyboard As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnHideKeyboard As System.Windows.Forms.Button
+End Class
---
+++ OnScreenKeyBoard/OnScreenKeyBoard/frmOnScreenKeyBoard.vb
@@ -0,0 +1,81 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: On Screen Keyboard usage TabTip.exe with VB.NET.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+#End Region
+
+Imports System.Runtime.InteropServices
+
+Public Class frmOnScreenKeyBoard
+
+    Const WM_SYSCOMMAND As Int32 = 274
+    Const SC_CLOSE As UInt32 = 61536
+
+    '// WinAPI32 From VB6. (API = Application Programming Interface)
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
+    Private Shared Function PostMessage( _
+        ByVal hWnd As IntPtr, _
+        ByVal Msg As UInteger, _
+        ByVal wParam As IntPtr, _
+        ByVal lParam As IntPtr _
+        ) As Boolean
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
+    Private Shared Function FindWindow( _
+        ByVal lpClassName As String, _
+        ByVal lpWindowName As String _
+        ) As IntPtr
+    End Function
+
+    '// Instance Name Process
+    Private _P As Process
+
+    Private Sub HideKeyboard()
+        Dim KeyboardWnd As IntPtr = FindWindow("IPTip_Main_Window", Nothing)
+        PostMessage(KeyboardWnd.ToInt32(), WM_SYSCOMMAND, SC_CLOSE, 0)
+    End Sub
+
+    Private Sub btnShowKeyboard_Click(sender As Object, e As EventArgs) Handles btnShowKeyboard.Click
+        If _P Is Nothing OrElse _P.HasExited Then
+            If _P IsNot Nothing AndAlso _P.HasExited Then _P.Close()
+            '// Sometimes TabTip may be hidden.
+            Call KillProcess()
+            _P = Process.Start("C:\Program Files\Common Files\microsoft shared\ink\TabTip.exe")
+            '/ Focus to the textbox
+            TextBox1.Focus()
+        End If
+    End Sub
+
+    Private Sub KillProcess()
+        For Each p As Process In Process.GetProcesses
+            If String.Compare(p.ProcessName, "TabTip", True) = 0 Then
+                p.Kill()
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub btnHideKeyboard_Click(sender As System.Object, e As System.EventArgs) Handles btnHideKeyboard.Click
+        Call HideKeyboard()
+    End Sub
+
+    Private Sub frmOnScreenKeyBoard_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Call HideKeyboard()
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+End Class
